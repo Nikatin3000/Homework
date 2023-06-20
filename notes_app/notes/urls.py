a8f57db968ee0c1828d82d3d6d7910a28c707c73
Vadim Kryuk <vadimkryuk145@gmail.com>
2023-06-20 12:07:52 +0300

    lesson 39

--- notes_app/notes/urls.py
+++ notes_app/notes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('hello/', views.index, name='index'),
+    path('', views.notes, name='notes'),
 ]
--- notes_app/notes/views.py
+++ notes_app/notes/views.py
@@ -1,6 +1,11 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello from Notes app")
+
+def notes(request):
+    notes_list = ['note_1', 'note_2', 'note_3', 'note_4', 'note_5']
+    context = {'notes_list': notes_list}
+    return render(request, 'notes/index.html', context)
