bd19f31306eeb3f86dd479d3395b97b67893248d
Vadim Kryuk <vadimkryuk145@gmail.com>
2023-06-23 11:52:21 +0300

    lesson 40

--- notes_app/notes/models.py
+++ notes_app/notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Notes(models.Model):
+    categories = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    reminder = models.DateTimeField('reminder')
+
+    def __str__(self):
+        return f"{self.categories}, {self.title}, {self.text}, {self.reminder}"
