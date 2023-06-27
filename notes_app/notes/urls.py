e68eaa0678cd99159117a77bbb1b89166de5a836
Vadim Kryuk <vadimkryuk145@gmail.com>
2023-06-27 10:45:30 +0300

    lesson 41

--- notes_app/notes/urls.py
+++ notes_app/notes/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'notes'
+
 urlpatterns = [
-    path('hello/', views.index, name='index'),
     path('', views.notes, name='notes'),
+    path('create/', views.create_note, name='create'),
+    path('<int:note_id>/delete/', views.delete_note, name='delete'),
+    path('<int:note_id>/correct/', views.correct_note, name='correct'),
 ]
--- notes_app/notes/views.py
+++ notes_app/notes/views.py
@@ -1,11 +1,52 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Notes, Categories
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .forms import NotesForm
 
 
-def index(request):
-    return HttpResponse("Hello from Notes app")
+# def index(request):
+#     return HttpResponse("Hello from Notes app")
 
 def notes(request):
     notes = Notes.objects.select_related('categories').all()
     return render(request, 'notes/index.html', {'data': notes})
+
+def create_note(request):
+    if request.method == 'POST':
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            # form.save()
+            q = Notes(categories=form.cleaned_data['categories'], title=form.cleaned_data['title'],
+                         text=form.cleaned_data['text'], reminder=form.cleaned_data['reminder'])
+            q.save()
+
+            return HttpResponseRedirect('/notes/')
+    else:
+        form = NotesForm()
+
+    return render(request, 'notes/create_note.html', {'form': form})
+
+
+def delete_note(request, note_id):
+        if request.method == 'POST':
+            note = Notes.objects.get(pk=note_id)
+            note.delete()
+            return redirect('/notes/')
+        else:
+            return HttpResponseRedirect('/notes/')
+
+
+def correct_note(request, note_id):
+    note = get_object_or_404(Notes, pk=note_id)
+    if request.method == 'POST':
+        form = NotesForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/notes/')
+    else:
+        form = NotesForm(instance=note)
+
+    return render(request, 'notes/correct_note.html', {'form': form})
